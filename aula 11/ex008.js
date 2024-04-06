a4afa19314955ee55feda575b0ead84196b8216d
Reginaldo Moreira <139932119+Reginaldo-Moreira-dev@users.noreply.github.com>
2024-04-06 07:46:40 -0300

    refazendo codigo

---
+++ aula 11/ex008.js
@@ -0,0 +1,2 @@
+var vel = 89;
+console.log('A velocidade do seu carro é ${vel}km/h')
---
+++ aula 11/node_16f4455f0ecf5.tmp.js
@@ -0,0 +1,6 @@
+var vel = 89;
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) { //condicão simples
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança`)
